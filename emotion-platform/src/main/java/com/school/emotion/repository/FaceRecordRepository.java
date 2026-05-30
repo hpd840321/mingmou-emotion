badeadbbf19a1fc94fe3c2bbd8500037c427a96b
hpd840321 <hpd84_0321@163.com>
2026-05-31 01:32:41 +0800

    feat: add findByLibFaceId to FaceRecordRepository

--- emotion-platform/src/main/java/com/school/emotion/repository/FaceRecordRepository.java
+++ emotion-platform/src/main/java/com/school/emotion/repository/FaceRecordRepository.java
@@ -2,9 +2,15 @@ package com.school.emotion.repository;
 
 import com.school.emotion.model.entity.FaceRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import java.util.List;
+import java.util.Optional;
 
 public interface FaceRecordRepository extends JpaRepository<FaceRecord, Long> {
     List<FaceRecord> findByClassImageId(Long classImageId);
     List<FaceRecord> findByStudentId(Long studentId);
+    Optional<FaceRecord> findByLibFaceId(String libFaceId);
+
+    @Query("SELECT DISTINCT fr.classImage.id FROM FaceRecord fr WHERE fr.errorMessage IS NOT NULL")
+    List<Long> findClassImageIdsWithFailedFaces();
 }
